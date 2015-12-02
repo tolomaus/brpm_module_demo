7a5e0664d9f1184244252942ebdc57348bfbe003
tolomaus <niek.bartholomeus@gmail.com>
2015-12-02 20:55:40 +0100

    set the request url in the ServiceNow ticket when a request is created from ServiceNow add the logs to the servicenow ticket when a step completes or moves to problem

--- lib/integrations/brpm/process_event_handler_event.rb
+++ lib/integrations/brpm/process_event_handler_event.rb
@@ -4,7 +4,10 @@ require_relative "../../jira_mappings"
 def process_event(event)
   @brpm_rest_client = BrpmRestClient.new("http://#{ENV["EVENT_HANDLER_BRPM_HOST"]}:#{ENV["EVENT_HANDLER_BRPM_PORT"]}/brpm", ENV["EVENT_HANDLER_BRPM_TOKEN"])
 
-  if event.has_key?("request")
+  if event.has_key?("step")
+    BrpmAuto.log "The event is for a step #{event["event"][0]}..."
+    process_step_event(event)
+  elsif event.has_key?("request")
     BrpmAuto.log "The event is for a request #{event["event"][0]}..."
     process_request_event(event)
   elsif event.has_key?("run")
@@ -16,11 +19,36 @@ def process_event(event)
   end
 end
 
+def process_step_event(event)
+  if event["event"][0] == "create"
+    step = event["step"].find { |item| item["type"] == "new" }
+
+    BrpmAuto.log "Step '#{step["name"][0]}' created"
+  elsif event["event"][0] == "update"
+    step_old_state = event["step"].find { |item| item["type"] == "old" }
+    step_new_state = event["step"].find { |item| item["type"] == "new" }
+
+    if step_old_state["aasm-state"][0] != step_new_state["aasm-state"][0] or step_new_state["aasm-state"][0] == "complete" #TODO bug when a request is moved to complete the old state is also reported as complete
+      BrpmAuto.log "Step '#{step_new_state["name"][0]}' moved from state '#{step_old_state["aasm-state"][0]}' to state '#{step_new_state["aasm-state"][0]}'"
+
+      if step_new_state["aasm-state"][0] == "complete" or step_new_state["aasm-state"][0] == "problem"
+        if step_new_state["property-values"][0].has_key?("add-logs-to-request-params") and step_new_state["property-values"][0]["add-logs-to-request-params"] == "true"
+          add_logs_to_ticket_in_servicenow(step_new_state)
+        end
+      end
+    end
+  end
+end
+
 def process_request_event(event)
   if event["event"][0] == "create"
     request = event["request"].find { |item| item["type"] == "new" }
 
     BrpmAuto.log "Request '#{request["name"][0]}' created"
+
+    if request["wiki-url"][0] == "ServiceNow change request"
+      add_link_to_ticket_in_servicenow(request)
+    end
   elsif event["event"][0] == "update"
     request_old_state = event["request"].find { |item| item["type"] == "old" }
     request_new_state = event["request"].find { |item| item["type"] == "new" }
@@ -86,8 +114,18 @@ def process_plan_event(event)
   end
 end
 
+
 #################################
 # BRPM
+
+def get_default_params
+  params = {}
+  params["brpm_url"] = "http://#{ENV["EVENT_HANDLER_BRPM_HOST"]}:#{ENV["EVENT_HANDLER_BRPM_PORT"]}/brpm"
+  params["brpm_api_token"] = ENV["EVENT_HANDLER_BRPM_TOKEN"]
+
+  params["log_file"] = ENV["EVENT_HANDLER_LOG_FILE"]
+  params
+end
 
 def process_app_release_event(request)
   release_request_stage_name = "Release"
@@ -116,21 +154,17 @@ end
 #################################
 # JIRA
 
-def get_default_params
-  params = {}
+def get_default_params_for_jira
+  params = get_default_params
   params["SS_integration_dns"] = ENV["EVENT_HANDLER_JIRA_URL"]
   params["SS_integration_username"] = ENV["EVENT_HANDLER_JIRA_USERNAME"]
   params["SS_integration_password"] = ENV["EVENT_HANDLER_JIRA_PASSWORD"]
 
-  params["brpm_url"] = "http://#{ENV["EVENT_HANDLER_BRPM_HOST"]}:#{ENV["EVENT_HANDLER_BRPM_PORT"]}/brpm"
-  params["brpm_api_token"] = ENV["EVENT_HANDLER_BRPM_TOKEN"]
-
-  params["log_file"] = ENV["EVENT_HANDLER_LOG_FILE"]
   params
 end
 
 def update_tickets_in_jira_by_request(request)
-  params = get_default_params
+  params = get_default_params_for_jira
   params["request_id"] = (request["id"][0]["content"].to_i + 1000).to_s
 
   request_with_details = @brpm_rest_client.get_request_by_id(request["id"][0]["content"])
@@ -145,7 +179,7 @@ def update_tickets_in_jira_by_request(request)
 end
 
 def update_tickets_in_jira_by_run(run)
-  params = get_default_params
+  params = get_default_params_for_jira
   params["run_id"] = run["id"][0]["content"]
 
   BrpmAuto.log "Getting the stage of this run..."
@@ -158,7 +192,7 @@ def update_tickets_in_jira_by_run(run)
 end
 
 def create_release_in_jira(plan)
-  params = get_default_params
+  params = get_default_params_for_jira
   params["jira_release_field_id"] = ENV["EVENT_HANDLER_JIRA_RELEASE_FIELD_ID"]
   params["release_name"] = plan["name"][0]
 
@@ -166,7 +200,7 @@ def create_release_in_jira(plan)
 end
 
 def update_release_in_jira(old_plan, new_plan)
-  params = get_default_params
+  params = get_default_params_for_jira
   params["jira_release_field_id"] = ENV["EVENT_HANDLER_JIRA_RELEASE_FIELD_ID"]
   params["old_release_name"] = old_plan["name"][0]
   params["new_release_name"] = new_plan["name"][0]
@@ -175,10 +209,49 @@ def update_release_in_jira(old_plan, new_plan)
 end
 
 def delete_release_in_jira(plan)
-  params = get_default_params
+  params = get_default_params_for_jira
   params["jira_release_field_id"] = ENV["EVENT_HANDLER_JIRA_RELEASE_FIELD_ID"]
   params["release_name"] = plan["name"][0]
 
   BrpmScriptExecutor.execute_automation_script("brpm_module_jira", "delete_release", params)
+end
+#################################
+
+#################################
+# ServiceNow
+
+def get_default_params_for_servicenow
+  params = get_default_params
+  params["SS_integration_dns"] = ENV["EVENT_HANDLER_SERVICENOW_URL"]
+  params["SS_integration_username"] = ENV["EVENT_HANDLER_SERVICENOW_USERNAME"]
+  params["SS_integration_password"] = ENV["EVENT_HANDLER_SERVICENOW_PASSWORD"]
+
+  params
+end
+
+def add_link_to_ticket_in_servicenow(request)
+  request_with_details = @brpm_rest_client.get_request_by_id(request["id"][0]["content"])
+
+  request_params = RequestParams.new_for_request("#{ENV["BRPM_HOME"]}/automation_results", request_with_details["apps"][0]["name"], request_with_details["id"].to_i + 1000)
+
+  params = get_default_params_for_servicenow
+  params["change_request_id"] = request_params["change_request_id"]
+
+  params["fields"] = { "u_url_brpm_request" => "#{params["brpm_url"]}/requests/#{request_with_details["id"]}" }
+
+  BrpmScriptExecutor.execute_automation_script("brpm_module_servicenow", "update_change_request", params)
+end
+
+def add_logs_to_ticket_in_servicenow(step)
+  step_with_details = @brpm_rest_client.get_step_by_id(step["id"][0]["content"])
+
+  request_params = RequestParams.new_for_request("#{ENV["BRPM_HOME"]}/automation_results", step_with_details["installed_component"]["app"]["name"], step["request"]["id"].to_i + 1000)
+
+  params = get_default_params_for_servicenow
+  params["change_request_id"] = request_params["change_request_id"]
+
+  params["fields"] = { "u_string_brpm_log_note" => request_params["logs"].map { |k, v| "#{k}:\n#{v}"} }
+
+  BrpmScriptExecutor.execute_automation_script("brpm_module_servicenow", "update_change_request", params)
 end
 #################################
